c62b39f457333603c00a9b8a20174553488cd595
Arnaud Grosjean <arnaudg@momentfactory.com>
2023-09-05 15:44:53 -0400

    added the release build containing the dlls in the release

---
+++ exts/mf.ov.lidar_live_synth/mf/ov/lidar_live_synth/ogn/tests/TestOgnBeamToOusterUDPNode.py
@@ -0,0 +1,102 @@
+import omni.kit.test
+import omni.graph.core as og
+import omni.graph.core.tests as ogts
+import os
+
+
+class TestOgn(ogts.OmniGraphTestCase):
+
+    async def test_data_access(self):
+        from mf.ov.lidar_live_synth.ogn.OgnBeamToOusterUDPNodeDatabase import OgnBeamToOusterUDPNodeDatabase
+        test_file_name = "OgnBeamToOusterUDPNodeTemplate.usda"
+        usd_path = os.path.join(os.path.dirname(__file__), "usd", test_file_name)
+        if not os.path.exists(usd_path):
+            self.assertTrue(False, f"{usd_path} not found for loading test")
+        (result, error) = await ogts.load_test_file(usd_path)
+        self.assertTrue(result, f'{error} on {usd_path}')
+        test_node = og.Controller.node("/TestGraph/Template_mf_ov_lidar_live_synth_BeamToOusterUDPNode")
+        database = OgnBeamToOusterUDPNodeDatabase(test_node)
+        self.assertTrue(test_node.is_valid())
+        node_type_name = test_node.get_type_name()
+        self.assertEqual(og.GraphRegistry().get_node_type_version(node_type_name), 1)
+
+        def _attr_error(attribute: og.Attribute, usd_test: bool) -> str:
+            test_type = "USD Load" if usd_test else "Database Access"
+            return f"{node_type_name} {test_type} Test - {attribute.get_name()} value error"
+
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:azimuthRange"))
+        attribute = test_node.get_attribute("inputs:azimuthRange")
+        db_value = database.inputs.azimuthRange
+        expected_value = [-3.14159, 3.14159]
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:broadcast"))
+        attribute = test_node.get_attribute("inputs:broadcast")
+        db_value = database.inputs.broadcast
+        expected_value = False
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:execIn"))
+        attribute = test_node.get_attribute("inputs:execIn")
+        db_value = database.inputs.execIn
+        expected_value = 0
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:horizontalResolution"))
+        attribute = test_node.get_attribute("inputs:horizontalResolution")
+        db_value = database.inputs.horizontalResolution
+        expected_value = 2
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:ip_address"))
+        attribute = test_node.get_attribute("inputs:ip_address")
+        db_value = database.inputs.ip_address
+        expected_value = "127.0.0.1"
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:linearDepthData"))
+        attribute = test_node.get_attribute("inputs:linearDepthData")
+        db_value = database.inputs.linearDepthData
+        expected_value = []
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:numCols"))
+        attribute = test_node.get_attribute("inputs:numCols")
+        db_value = database.inputs.numCols
+        expected_value = 1
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:numRows"))
+        attribute = test_node.get_attribute("inputs:numRows")
+        db_value = database.inputs.numRows
+        expected_value = 1
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("inputs:port"))
+        attribute = test_node.get_attribute("inputs:port")
+        db_value = database.inputs.port
+        expected_value = 8037
+        actual_value = og.Controller.get(attribute)
+        ogts.verify_values(expected_value, actual_value, _attr_error(attribute, True))
+        ogts.verify_values(expected_value, db_value, _attr_error(attribute, False))
+
+        self.assertTrue(test_node.get_attribute_exists("outputs:execOut"))
+        attribute = test_node.get_attribute("outputs:execOut")
+        db_value = database.outputs.execOut
